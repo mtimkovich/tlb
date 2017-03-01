c775401e6d50043b17f3972d870571ed43ef48fe
Max Timkovich <mtimkovich@gmail.com>
2017-03-01 14:17:36 -0800

    move_mouse

---
+++ src/move_mouse.py
@@ -0,0 +1,26 @@
+"""
+This is for keeping Ernest's computer awake while playing Smash
+"""
+#!/usr/bin/python
+import autopy
+import time
+
+# Requires autopy from https://github.com/msanders/autopy
+
+def move(x, y):
+    try:
+        autopy.mouse.move(x, y)
+        time.sleep(1)
+    except ValueError:
+        pass
+
+while True:
+    x, y = autopy.mouse.get_pos()
+
+    move(x+5, y+5)
+    move(x+5, y-5)
+    move(x-5, y-5)
+    move(x-5, y+5)
+    move(x, y)
+
+    time.sleep(60)
